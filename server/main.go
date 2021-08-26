0766e46e9e9763051095a8a0192d2ba9f154a3a5
Charles Houser <cjohouser@gmail.com>
2021-08-26 09:51:04 -0700

    main cleanup

--- server/main.go
+++ server/main.go
@@ -20,41 +20,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("table not created %v", err)
 	}
-	/*
-		// fetch DB items as a typed slice of DBTodoItem
-		items, _ := db.SelectAllTodoItems()
-		log.Println(len(*items), "items in the database")
-
-		// declare a new DBTodoItem (with embedded TodoItem)
-		item := &data.DBTodoItem{&models.TodoItem{}}
-		item.Title = "test"
-
-		// insert into the DB
-		db.InsertTodoItem(item)
-		log.Printf("%s (done: %t) inserted into the database", item.Title, item.Done)
-
-		// demonstrate successful insert
-		items, _ = db.SelectAllTodoItems()
-		log.Println(len(*items), "items in the database")
-		item = (*items)[0]
-		log.Printf("%s (done: %t) found in database", item.Title, item.Done)
-
-		// update in DB
-		db.UpdateTodoItem(item)
-		log.Printf("%s (done: %t) updated in the database", item.Title, item.Done)
-
-		// demonstrate successful update
-		items, _ = db.SelectAllTodoItems()
-		log.Println(len(*items), "items in the database")
-		item = (*items)[0]
-		log.Printf("%s (done: %t) found in database", item.Title, item.Done)
-
-		// delete item
-		db.DeleteTodoItem(item)
-
-		// demonstrate successful deletion 	items, _ = db.SelectAllTodoItems()
-		log.Println(len(*items), "items in the database")
-	*/
-	log.Println("Listening Request")
+	log.Println("Listening for requests")
 	handleRequests(db)
 }
