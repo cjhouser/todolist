c337c2756077b5be0a25cdbd6657c859542740ca
Charles Houser <cjohouser@gmail.com>
2021-08-30 15:46:21 -0700

    memcache

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/CJHouser/tasklist/models"
+	"github.com/bradfitz/gomemcache/memcache"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
@@ -22,7 +23,7 @@ type env struct {
 }
 
 func main() {
-	log.Println("Opening database")
+	log.Println("Connecting to database")
 	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@/"+dbname)
 	if err != nil {
 		log.Fatalf("unable to open database %v", err)
@@ -30,21 +31,21 @@ func main() {
 	if _, err := db.Exec(sqlCreateTasksTable); err != nil {
 		log.Fatalf("unable to create table %v", err)
 	}
-
+	log.Println("Connecting to cache")
+	mc := memcache.New("localhost:11211")
+	// Dependency injection
 	env := &env{
-		tasks: models.TaskModel{DB: db},
+		tasks: models.TaskModel{DB: db, MC: mc},
 	}
-
-	myRouter := mux.NewRouter().StrictSlash(true)
-	// Middleware
-	myRouter.Use(logging)
 	// Routes
+	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/tasks", env.returnAllTasks)
 	myRouter.HandleFunc("/task", env.createTask).Methods("POST")
 	myRouter.HandleFunc("/task/{id}", env.updateTask).Methods("PUT")
 	myRouter.HandleFunc("/task/{id}", env.deleteTask).Methods("DELETE")
 	myRouter.HandleFunc("/task/{id}", env.returnSingleTask)
-
+	// Middleware
+	myRouter.Use(logging)
 	log.Println("Listening for requests")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
